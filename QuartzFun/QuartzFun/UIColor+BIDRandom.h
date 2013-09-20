ca3a90d73e51a73b7f4fead95aa93fd2b756810f
=Shaliemar Abucay <shlabucay@gmail.com>
2013-09-20 15:40:54 +0800

    Implemented code to create a random color.

--- QuartzFun/QuartzFun/UIColor+BIDRandom.h
+++ QuartzFun/QuartzFun/UIColor+BIDRandom.h
@@ -9,5 +9,5 @@
 #import <UIKit/UIKit.h>
 
 @interface UIColor (BIDRandom)
-
++(UIColor *) randomColor;
 @end
--- QuartzFun/QuartzFun/UIColor+BIDRandom.m
+++ QuartzFun/QuartzFun/UIColor+BIDRandom.m
@@ -9,5 +9,17 @@
 #import "UIColor+BIDRandom.h"
 
 @implementation UIColor (BIDRandom)
++ (UIColor *) randomColor
+{
+    static BOOL seeded = NO;
+    if (!seeded) {
+        seeded = YES;
+        srandom(time(NULL));
+    }
+    CGFloat red = (CGFloat)random() / (CGFloat)RAND_MAX;
+    CGFloat blue = (CGFloat)random() / (CGFloat)RAND_MAX;
+    CGFloat green = (CGFloat)random() / (CGFloat)RAND_MAX;
+    return [UIColor colorWithRed:red green:green blue:blue alpha:1.0f];
+}
 
 @end
