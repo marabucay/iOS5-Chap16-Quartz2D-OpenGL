2c2d56396e1abff7846be96dc3fae8de0f5368a1
=Shaliemar Abucay <shlabucay@gmail.com>
2013-09-23 10:45:31 +0800

    Finished quartz2D.

---
+++ GLFun/QuartzFun/BIDGLFunView.h
@@ -0,0 +1,13 @@
+//
+//  BIDGLFunView.h
+//  GLFun
+//
+//  Created by Maria Alice C. Lim on 9/20/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import "OpenGLES2DView.h"
+
+@interface BIDGLFunView : OpenGLES2DView
+
+@end
---
+++ GLFun/QuartzFun/BIDGLFunView.m
@@ -0,0 +1,13 @@
+//
+//  BIDGLFunView.m
+//  GLFun
+//
+//  Created by Maria Alice C. Lim on 9/20/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import "BIDGLFunView.h"
+
+@implementation BIDGLFunView
+
+@end
